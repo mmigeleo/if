aa1e9633a2c57ad04383b79602c2b4d35e523280
Mikayil <mmigeleo221002@gmail.com>
2023-11-18 23:00:09 +0400

    START

---
+++ start.js
@@ -0,0 +1,14 @@
+let user = prompt('Login');
+let password = prompt('Password');
+let message;
+
+
+if (user == 'admin' && password == '123456') {
+    message = 'Welcome, User!';
+} else if (user == 'Vahid' && password == '1234') {
+    message = 'Welcome, Vahid!';
+} else {
+    message = 'Login or Password is/are wrong!';
+}
+
+document.getElementById('start').innerHTML = outcome;
