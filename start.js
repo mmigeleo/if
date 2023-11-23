0581f3a605421bff456a120840571f5a8e32278c
Mikayil <mmigeleo221002@gmail.com>
2023-11-23 19:37:18 +0400

    Facebook change

--- start.js
+++ start.js
@@ -7,6 +7,14 @@ if (user == 'admin' && password == '123456') {
     message = 'Welcome, User!';
 } else if (user == 'Vahid' && password == '1234') {
     message = 'Welcome, Vahid!';
+} else if (user == 'Ayaz' && password == '1234') {
+    message = 'Welcome, Ayaz!';
+} else if (user == 'Murad' && password == '1234') {
+    message = 'Welcome, Murad!';   
+} else if (user == 'Elmaddin' && password == '1234') {
+    message = 'Welcome, Elmaddin!';
+} else if (user == 'Mikayil' && password == '1234') {
+    message = 'Welcome, Mikayil!';
 } else {
     message = 'Login or Password is/are wrong!';
 }
